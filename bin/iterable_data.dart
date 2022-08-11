e8d720103c1cc7f5a76b7feea77a401e85f6195b
Axmadjon Isaqov <67532499+axmadjon2000@users.noreply.github.com>
2022-08-11 22:53:54 +0500

    stream

--- bin/iterable_data.dart
+++ bin/iterable_data.dart
@@ -1,7 +1,20 @@
 import 'dart:io';
 
-void main() {
-  print(getIterable());
+import 'dart:async';
+
+import 'package:stream_channel/stream_channel.dart';
+
+void main() async {
+  StreamController<String> controller = StreamController<String>();
+  StreamSubscription subscription = controller.stream.listen((event) {
+    print(event);
+  });
+
+  StreamChannel channel = StreamChannel(controller.stream, controller.sink);
+  for (var i = 0; i <= 100; i++) {
+    await Future.delayed(Duration(seconds: 1));
+    channel.sink.add("index::$i");
+  }
 }
 
 Iterable<int> getIterable() sync* {
